cd70078ea398981d71d8cf35bb3f678e334e1045
buns919 <sbehun07+1@gmail.com>
2018-09-29 17:42:36 -0400

    added components and colliders for paddle, walls and lose area

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle;
+    [SerializeField] float startingSpeed = 40f;
+    float startingXDirection;
+
+    bool isBallLaunched = false;
+    Vector2 paddleToBallVector;
+
+    // Use this for initialization
+    void Start () {
+        // get offset from paddle to ball so we can lock the ball on top of the paddle before launch
+        paddleToBallVector = transform.position - paddle.transform.position;
+
+        // create random starting x direction
+        startingXDirection = Random.Range(-10f, 10f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Lock the ball to the paddle to start
+        if (!isBallLaunched) {
+            LockBallToPaddle();
+        }
+
+
+	}
+
+    void LockBallToPaddle() {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+        if (Input.GetMouseButtonDown(0)) {
+            LaunchBall();
+        }
+    }
+
+    void LaunchBall() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector2 startingForce = new Vector2(startingXDirection, startingSpeed);
+        rb.velocity = startingForce;
+        isBallLaunched = true;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float paddleMinX = 1f;
+    [SerializeField] float paddleMaxX = 15f;
+
+    [SerializeField] float screenWidthUnits = 16f;
+    float startingYPos;
+
+    // Use this for initialization
+    void Start () {
+        startingYPos = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        movePaddle();
+
+    }
+
+    void movePaddle() {
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * screenWidthUnits);
+
+        float clampedX = Mathf.Clamp(mousePosInUnits, paddleMinX, paddleMaxX);
+        Vector2 paddlePos = new Vector2(clampedX, startingYPos);
+
+
+        transform.position = paddlePos;
+    }
+}
